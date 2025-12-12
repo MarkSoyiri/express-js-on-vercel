7395860ec37194059210b211f0378ff9ad774c5d
MarkSoyiri <marksoyiri001@gmail.com>
2025-12-12 12:34:29 +0000

    up

--- controllers/authController.js
+++ controllers/authController.js
@@ -46,7 +46,9 @@ const login = async (req, res) => {
         const token = signToken({ id: user._id });
         res.status(200).json({user: {id: user._id}, token});
     } catch (err) {
+        res.status(401).json({error:err.message});
         next(err)
+        
     }
 }
 
