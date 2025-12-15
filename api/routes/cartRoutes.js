940e93858bcadf84040d031a46952228af506578
MarkSoyiri <marksoyiri001@gmail.com>
2025-12-15 06:37:36 +0000

    up

--- api/routes/cartRoutes.js
+++ api/routes/cartRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { getCart, addItem, updateItem, clearCart } = require("../controllers/cartController");
 const authM = require("../middleware/authMiddleware");
 
-router.get('/', authM, getCart);
-router.post('/add', authM, addItem);
-router.post('/update', authM, updateItem);
-router.post('/clear', authM, clearCart);
+router.get('/cart', authM, getCart);
+router.post('/cart/add', authM, addItem);
+router.post('/cart/update', authM, updateItem);
+router.post('/cart/clear', authM, clearCart);
 
 module.exports = router;
